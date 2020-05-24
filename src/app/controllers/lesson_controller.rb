ffbf04f4d404689b7c407633bb3aa69416fdc3e4
H-2910 <h-2910@H-2910.local>
2020-05-24 22:33:37 +0900

    [chapter3.3] step7〜18の実行

--- src/app/controllers/lesson_controller.rb
+++ src/app/controllers/lesson_controller.rb
@@ -23,4 +23,52 @@ class LessonController < ApplicationController
   def step6
     render plain: flash[:notice]
   end
+
+  def step7
+    @price = (2000 * 1.08).floor
+  end
+
+  def step8
+    @price = 1000
+    render action: "step7"
+  end
+
+  def step9
+    @comment = "<script>alert('危険!')</script>こんにちは"
+  end
+
+  def step10
+    @comment = "<strong>安全なHTML</strong>"
+  end
+
+  def step11
+    @population = 704414
+    @surface = 141.31
+  end
+
+  def step12
+    @time = Time.current
+  end
+
+  def step13
+    @population = 127767944
+  end
+
+  def step14
+    @message = "ごきげんいかが？¥nRailsの勉強をがんばりましょう。"
+  end
+
+  def step15
+  end
+
+  def step16
+  end
+
+  def step17
+    @zaiko = 10
+  end
+
+  def step18
+    @items = {"フライパン"=>2680, "ワイングラス"=>2550, "ペッパーミル"=>4515, "ピーラー"=>945}
+  end
 end
